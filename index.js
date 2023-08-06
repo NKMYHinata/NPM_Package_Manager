26ff38e0e0f3611cd820d83c2ff8a37ec67430e7
MinatoAqua <xiriy@outlook.com>
2023-08-06 20:01:22 +0800

    first

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('hello 1024Code')
+const express = require('express');
+const { exec } = require('child_process');
+const app = express();
+const PORT = 8080;
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+    
+    const url = `http://localhost:${PORT}/main.html`;
+    switch (process.platform) {
+        case 'darwin':
+            exec(`open ${url}`);
+            break;
+        case 'win32':
+            exec(`start ${url}`);
+            break;
+        default:
+            exec(`xdg-open ${url}`);
+    }
+});
